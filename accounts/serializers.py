c9623512f40f6a07bf86576b8b425f873ee2c38a
= <=>
2026-02-17 00:48:29 +0200

    feat: add accounts registration and jwt endpoints

---
+++ accounts/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password")
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from accounts.serializers import UserRegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = (AllowAny,)
