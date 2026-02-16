f0f0e0b014008bc461e2a6b784242a7cd6e72ac4
= <=>
2026-02-17 00:43:23 +0200

    feat: add api viewsets and routing

--- airport/urls.py
+++ airport/urls.py
@@ -1,3 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from airport.views import (
+    AirportViewSet,
+    RouteViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    CrewViewSet,
+    FlightViewSet,
+    OrderViewSet,
+)
+
+router = DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crew", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet, basename="orders")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
